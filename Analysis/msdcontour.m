ae15c444483a11f1cd9b75dc951ba0efcb91b324
John Palmer <jpal8929@physics.usyd.edu.au>
2013-10-09 11:44:15 +1100

    Fix msdcontour script
    The msdcontour script may have issues in cases were there were no
    patterns present - set a minimum limit and then plot a NaN

--- Analysis/msdcontour.m
+++ Analysis/msdcontour.m
@@ -1,12 +1,13 @@
-directory='out';
-files=dir('*');
+directory='out/';
+files=dir('out/*');
 min1=1;
 max1=2;
-spacing1=0.05;
+spacing1=1;
 min2=1;
-max2=1;
-spacing2=0.05;
-wevals min1:spacing1:max1;
+max2=2;
+spacing2=1;
+let minlen = 10;
+wevals=min1:spacing1:max1;
 wivals=min2:spacing2:max2;
 output=zeros(length(wevals),length(wivals));
 for x=1:length(files) 
@@ -17,15 +18,19 @@ for x=1:length(files)
         params=sscanf(fname,'%f,%f');
         we=params(1); 
         wi=params(2);
-        wedex=int32(((we-1)/spacing)+1);
-        widex=int32(((abs(wi)-1)/spacing)+1);
+        wedex=int32(((we-min1)/spacing1)+1);
+        widex=int32(((abs(wi)-min2)/spacing2)+1);
         %get actual data
         data=importdata(sprintf('%s%s',directory,fname));
-        alpha=polyfit(log(1:length(data))',log(data),1);
-        output(wedex,widex)=alpha(1);
+        if length(data>minlen)
+            alpha=polyfit(log(1:length(data))',log(data),1)
+            output(wedex,widex)=alpha(1);
+        else
+            output(wedex,widex)=NaN;
+        end
         
     end
 end
-contour(wivals,wevals,output)
+contourf(wivals,wevals,output)
 set(gca,'YDir','normal'); %so that the plot looks normal
 colorbar

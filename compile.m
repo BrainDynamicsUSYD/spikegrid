c6805ef5e69773ad309f766cabc0fcf39f5a00c1
John Palmer <jpal8929@physics.usyd.edu.au>
2013-10-15 11:17:33 +1100

    fix compilation under matlab
    matlab requires you to use LDFLAGS and CFLAGS properly.  Fixed

--- compile.m
+++ compile.m
@@ -1,1 +1,1 @@
-mex CFLAGS="-g -O3 -std=gnu99 -lm -lpng -fPIC -shared -DMATLAB"  conductance.c coupling.c parameters.c STDP.c STD.c movie.c
+mex CFLAGS="-g -O3 -std=gnu99 -fPIC -shared -DMATLAB" LDFLAGS="-lm -lpng"  conductance.c coupling.c parameters.c STDP.c STD.c movie.c

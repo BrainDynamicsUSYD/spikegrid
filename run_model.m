02a0b823ddee9db652fca5861aff52632a182535
john pamer <thepalm2@gmail.com>
2014-05-13 14:22:17 +1000

    update todo and run_model

--- run_model.m
+++ run_model.m
@@ -1,10 +1,12 @@
 compile;
 precision = 'single';
 data =((rand(100)*25)-80);
+datalayer2 =((rand(100)*25)-80);
 time=1;
 
 if strcmp(precision,'single')==1
     data = single(data);
+    datalayer2 = single(datalayer2);
     time = single(time);
 end
 
@@ -14,14 +16,15 @@ hV = imagesc(data ,[-80 -55]); colorbar;
 hT = title('Time: 1');
 
 % Excitatory conductance
-[data2, gE] =conductance(data,'gE');
+[data2,datalayer22, gE] =conductance(data,datalayer2,'gE');
 figure(2);
 hG = imagesc(gE);
 
 while time<1000
     time=time+1;
-    [data2, gE] =conductance(data,'gE');
+    [data2, datalayer22,gE] =conductance(data,datalayer2,'gE');
     data=data2;
+    datalayer2=datalayer22;
     if (mod(time,10)==0)
         set(hV,'CData',data);
         set(hT,'String',sprintf('Time: %.1f',time));

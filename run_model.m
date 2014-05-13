9324bbb8f5033cae33ec6a5e5fb0c336401da4e8
Adam Keane <keane@node00.physics.usyd.edu.au>
2014-05-13 11:33:21 +1000

    Made the MATLAB plotting faster

--- run_model.m
+++ run_model.m
@@ -1,19 +1,31 @@
-compile
-data=single((rand(100)*25)-80);
-time=0
+compile;
+precision = 'single';
+data =((rand(100)*25)-80);
+time=1;
+
+if strcmp(precision,'single')==1
+    data = single(data);
+    time = single(time);
+end
+
+% Membrane potential
+figure(1);
+hV = imagesc(data ,[-80 -55]); colorbar;
+hT = title('Time: 1');
+
+% Excitatory conductance
+[data2, gE] =conductance(data,'gE');
+figure(2);
+hG = imagesc(gE);
+
 while time<1000
     time=time+1;
-    [data2 gE] =conductance(data,'gE');
+    [data2, gE] =conductance(data,'gE');
     data=data2;
-    if (mod (time,10)==0)
-        figure(1)
-        imagesc(data ,[-80 -55])
-        colorbar
-        title(time)
-        figure(2)
-        imagesc(gE)
-        drawnow
-        disp(time)
-        
+    if (mod(time,10)==0)
+        set(hV,'CData',data);
+        set(hT,'String',sprintf('Time: %.1f',time));
+        set(hG,'CData',gE);  
+        pause(0.01);
     end
 end

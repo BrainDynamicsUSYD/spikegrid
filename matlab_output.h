41a4290d58fd365c4f4463572a5cd2f5c67a1bef
John Palmer <jpal8929@physics.usyd.edu.au>
2014-07-01 17:27:41 +1000

    make the compile work

--- matlab_output.h
+++ matlab_output.h
@@ -1,6 +1,6 @@
 #ifdef MATLAB
 #include "matlab_includes.h"
 #include "output.h"
-mxArray* outputToMxArray(const tagged_array input);
+mxArray* outputToMxArray(const output_s input);
 void outputExtraThings(mxArray* plhs[],int nrhs,const mxArray* prhs[]);
 #endif //matlab

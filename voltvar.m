640c9f16c4eca735ca07394c1b7a779ea0b68d2b
John Palmer <jpal8929@physics.usyd.edu.au>
2014-11-05 14:50:04 +1100

    update voltvar

--- voltvar.m
+++ voltvar.m
@@ -1,12 +1,13 @@
 %calculates variances in voltage - incredibly slow as there is a rather crazy amount of data to process
 for jobno=1:40
     jobno
+    stepsize=500;
     data=importdata(sprintf('job-%i/2.txt',jobno));
     res=[];
-    parfor step=1:99
+    parfor step=1:19
         tdata=[];
-        baset=step*100;
-        for t = 2:100
+        baset=step*stepsize;
+        for t = 2:stepsize
             tdata(t,:,:)=data(((t-1)*100:t*100)+baset*100,:);
         end
         vars=[];
@@ -17,5 +18,7 @@ for jobno=1:40
         end
         res(step) =  mean(mean(vars));
     end
+    vars=[];
+    tdata=[];
     save(sprintf('job-%f.mat',jobno));
 end

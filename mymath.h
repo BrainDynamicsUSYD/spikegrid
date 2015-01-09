e75d85fbe7a7b4d5b1f3d36a8614b72ebe5c1a5f
john palmer <j.palmer@physics.usyd.edu.au>
2015-01-09 14:39:01 +1100

    improve mymath.h comments and add cos

--- mymath.h
+++ mymath.h
@@ -1,9 +1,13 @@
 /// \file
+///This file produces the correct math functions for the compute_float type we are using.
+///The more common solution would be tgmath.h - however
+///tgmath.h produces lots of fun warnings about float <-> double <-> complex conversion.
+///So instead, we just do things manually.
 #include <math.h>
 #ifdef FAST
-//tgmath.h produces lots of fun warnings about float <-> double <-> complex conversion.  As a result, this is the new solution
-#define exp(x) expf(x)
+#define exp(x)  expf(x)
 #define fabs(x) fabsf(x)
-#define sin(x) sinf(x)
+#define sin(x)  sinf(x)
 #define ceil(x) ceilf(x)
+#define cos(x)  cosf(x)
 #endif

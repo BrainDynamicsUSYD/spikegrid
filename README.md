d51c5d41106f78e758115404cbd94bb8d9773769
john palmer <j.palmer@physics.usyd.edu.au>
2015-04-10 10:09:44 +1000

    use config.mk to hide some makefile complexity

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ John and Adams spiking conductance based model with STDP.
 #Dependencies
 1. OpenCV - used for visualization and some output - this can be disabled in the makefile, but many features will be lost
 2. Newish version of GCC - see here for how to access it at physics https://github.com/BrainDynamicsUSYD/neurofield/wiki/Getting-GCC-in-Physics
+3. inotify-tools - for `make watch` (build as soon as a file is changed)
 
 Following the instructions for setting up GCC should also pull in a version of openCV that I have compiled.
 
---
+++ config.mk
@@ -0,0 +1,49 @@
+##########
+#Makefile options:
+#########
+#export SPEEDFLAG=-DFAST #comment out this line for double instead of float (will make code slower)
+export DEFINES=-DOPENCV
+##########
+#set up some variables for compiling
+##########
+ifeq ($(CC),clang)
+	export CFLAGS= -g -Wno-padded -Wno-missing-prototypes -Wno-missing-variable-declarations -Weverything -pedantic  -Ofast -Wno-documentation-unknown-command -Wno-covered-switch-default 
+else #gcc
+	optflags=  -Ofast -msse -msse2 -msse3 -funsafe-loop-optimizations -mtune=native -march=native  -floop-interchange -ftree-loop-optimize -floop-strip-mine -floop-block -flto  -fassociative-math -fno-signed-zeros -freciprocal-math -ffinite-math-only -fno-trapping-math -ftree-vectorize
+	extrawarnings=-Wstrict-aliasing -fstrict-aliasing   -Wshadow  -Wconversion -Wdouble-promotion -Wformat=2 -Wunused -Wuninitialized -Wfloat-equal -Wunsafe-loop-optimizations -Wcast-qual -Wcast-align -Wwrite-strings  -Wlogical-op  -Wvector-operation-performance -Wno-pragmas
+	extraextrawarnings=-Wsuggest-attribute=pure  -Wsuggest-attribute=noreturn -Wstrict-overflow=4
+	export CFLAGS=-g -ggdb -Wall -Wextra  ${optflags} ${extrawarnings} ${extraextrawarnings} -Werror-implicit-function-declaration -fno-builtin-malloc
+	cspecificwarnings= -Wjump-misses-init 
+endif
+
+ifeq ($(MATLAB),yes) #this has to be an if due to errors
+	#now when we are using matlab, everything is way too hard
+	#this is mainly because matlab ships its own opencv libraries.
+	#There are several problems with this:
+	# 1. The matlab version is old and doesn't have all libraries
+	# 2. Using any of my installed opencv libs will make the code not run.
+	#So there is a rather ridiculous amount of hackery here
+
+	#First find where matlab is - we need to use readlink and then dirname as matlab is almost always symlinked
+	export matlabdir=$(shell dirname $$(readlink -f $$(which matlab)))
+	#these C and ld flags are stolen from mex-v
+	export MATLABCFLAGS= -DMX_COMPAT_32 -D_GNU_SOURCE -DMATLAB_MEX_FILE -DMATLAB -I"${matlabdir}/../extern/include" -I"${matlabdir}/../simulink/include"
+	export ADAMMATLABCFLAGS= -DMX_COMPAT_32 -D_GNU_SOURCE -DMATLAB_MEX_FILE -I"${matlabdir}/../extern/include" -I"${matlabdir}/../simulink/include"
+	export MATLABLDFLAGS=  -L"${matlabdir}/glnxa64" -lmx -lmex -lmat -lm -lstdc++
+	#here some hackery - use pkg-config to learn the names of the libs but then find to only get the ones matlab has a copy of
+	export matlabopencvldflags=$(shell for x in $$(pkg-config --libs opencv); do  find ${matlabdir}/glnxa64/ -name $$(basename -- $$x)\*  ; done)
+	#have to set rpath in the linker to get the right libs to link.  Also add in the extra flags
+	export matlabopencvldflags:= -Wl,-rpath -Wl,${matlabdir} ${matlabopencvldflags} $(shell pkg-config --libs-only-l opencv)
+endif
+#set up some non-matlab variables
+export DEBUGFLAGS= -g -std=gnu11 ${DEFINES}
+export CXXDEBUGFLAGS= -g --std=c++11 ${DEFINES}
+export CLIBFLAGS= -fPIC -shared
+export LDFLAGS= -lm -g
+export opencvcflags=$(shell  pkg-config --cflags opencv)
+export opencvldflags=$(shell  pkg-config --libs opencv)
+CFLAGS += ${SPEEDFLAG} ${DEFINES} -fno-omit-frame-pointer
+export CXXFLAGS:=${CFLAGS} #use := to create an actual copy
+CFLAGS += --std=gnu11 ${cspecificwarnings}
+CXXFLAGS += --std=c++11
+

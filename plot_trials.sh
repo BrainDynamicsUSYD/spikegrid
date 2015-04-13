1ce0d26539fde1c1c985ae1f9265c566cfbe1254
John Palmer <jpal8929@physics.usyd.edu.au>
2015-04-13 10:03:07 +1000

    updated plotting script

--- plot_trials.sh
+++ plot_trials.sh
@@ -1,10 +1,11 @@
 vars=$(
 for x in job.o* 
 do
-    jobno=$(echo $x | tr '-' ' ' | awk '{print $2}')
-    first=$(tail -1 $x | awk '{print $1}' | tr  '=' ' ' | awk '{print $2}')
-    second=$(tail -1 $x | awk '{print $2}' | tr  '=' ' ' | awk '{print $2}')
-    echo  $jobno  $first $second
+    #jobno=$(echo $x | tr '-' ' ' | awk '{print $2}')
+    #first=$(tail -1 $x | awk '{print $1}' | tr  '=' ' ' | awk '{print $2}')
+    #second=$(tail -1 $x | awk '{print $2}' | tr  '=' ' ' | awk '{print $2}')
+    #echo  $jobno  $first $second
+    tail -1 $x
 done | sort -n) 
-echo "$vars" V | tail
-echo -e "plot '-' using 1:2  , '-' using 1:3  \n" "$vars" "\n" e "\n""$vars"| gnuplot -persist
+echo "$vars" V 
+echo -e "plot '-' using 1:2  smooth unique , '-' using 1:3 smooth unique \n" "$vars" "\n" e "\n""$vars"| gnuplot -persist

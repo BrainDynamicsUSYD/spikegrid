099261ca985529764085f5d54802844ec8fe450a
Adam Keane <clubkeano@gmail.com>
2015-05-14 14:09:14 +1000

    Single layer now works, yay!

--- yossarian.c
+++ yossarian.c
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include "init.h" //so I can find out the amount of ram to create
 #include "utils.h"
+#include "newparam.h"
 ///Creates the file for submitting the yossarian job
 /// @param outfile The output filename.
 /// @param sweep The parameter being swept over (only needed for the count to know how many jobs to submit)
-void createyossarianfile (const char* const outfile,const sweepable sweep)
+void createyossarianfile (const char* const outfile,const sweepable sweep,const parameters const L1,const parameters const L2)
 {
     FILE* file = fopen(outfile,"w");
     char cwd[1024];
@@ -16,7 +17,7 @@ void createyossarianfile (const char* const outfile,const sweepable sweep)
         printf("error getting current directory - exiting");
         exit(EXIT_FAILURE);
     }
-    model* dummy __attribute__((unused))= setup(DualLayerModelIn,DualLayerModelEx,ModelType,-1,-1,1);
+    model* dummy __attribute__((unused))= setup(L1,L2,ModelType,-1,-1,1);
     int MemInMB = (int)(((double)total_malloced) * 1.1 / 1024.0 / 1024.0);
     fprintf(file,""
 "#!/bin/csh\n"

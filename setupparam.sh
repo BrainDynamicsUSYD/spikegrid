e21cc9403b36a31b324a4a40e6a9e195b1a55021
john palmer <j.palmer@physics.usyd.edu.au>
2015-07-15 13:08:36 +1000

    add a default choice to the setupparam script

--- setupparam.sh
+++ setupparam.sh
@@ -2,6 +2,9 @@
 files=config/*
 actualfiles=$(for var in config/*; do echo $var;basename $var ;done)
 FILE=$(dialog --title "select a parameter file" --stdout --title "select a parameter file" --menu  "test" $(( $(tput lines) -10)) $(($(tput cols)-5)) $(( $(tput lines) -15))  $actualfiles )
+if [ -z "${FILE}"]; then
+    FILE='config/parametersJOHN-travelling.h'
+fi
 echo '//disable warnings about float conversion in this file only
 #ifdef __clang__
 #pragma clang diagnostic push

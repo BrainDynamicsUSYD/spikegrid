8e80356e0286b634b176e7f69d9bb6c4010c249f
john palmer <j.palmer@physics.usyd.edu.au>
2015-08-04 10:41:55 +1000

    make phi use coord

--- phi.c
+++ phi.c
@@ -4,11 +4,12 @@
 Compute_float* CreatePhiMatrix(void)
 {
     Compute_float* PhiMat = calloc(sizeof(Compute_float),grid_size*grid_size);
-    for (unsigned int x = 0;x<grid_size;x++)
-    { 
-        for (unsigned int y = 0;y<grid_size;y++)
+    for (Neuron_coord x = 0;x<grid_size;x++)
+    {
+        for (Neuron_coord y = 0;y<grid_size;y++)
         {
-            PhiMat[x*grid_size+y] = (x*grid_size+y)*2*M_PI/(grid_size*grid_size);
+            const size_t idx = grid_index((coords){.x=x,.y=y});
+            PhiMat[idx] = (Compute_float)idx*2*M_PI/(grid_size*grid_size);
         }
     }
     return PhiMat;

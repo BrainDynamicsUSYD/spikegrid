2b0fd3f10a8d43510f4561ab39e2d18e50adab97
john palmer <j.palmer@physics.usyd.edu.au>
2015-09-18 12:29:37 +1000

    make everything much more sensitive

--- Analysis/params.fs
+++ Analysis/params.fs
@@ -4,7 +4,7 @@ let size=100
 let mods = 
     let bseq = -3::-2::-1::0::1::2::3::[]
     bseq |> List.collect (fun x -> bseq |> List.map (fun y -> x,y))
-let minsize=3
+let minsize=0
 type Class = 
     |Firing
     |Grouped of int

f288256dd0042b7b9d3c563b5467a15f6d600642
john palmer <j.palmer@physics.usyd.edu.au>
2016-05-25 16:35:44 +1000

    more comments and fix some more warnings

--- timing.c
+++ timing.c
@@ -13,10 +13,10 @@ void timertick(const size_t timesteps,const size_t totaltimesteps)
     clock_gettime(CLOCK_MONOTONIC,&curtime);
     Compute_float nanosecondsNOW = (Compute_float)curtime.tv_nsec + (Compute_float)(curtime.tv_sec)*(Compute_float)1e9; //This is the most ridiculous stupid api ever.  Just use a bigger data type people
     Compute_float timeperstep = 1.0/((nanosecondsNOW-starttimeNS) /(Compute_float)timesteps/(Compute_float)1e9);
-    Compute_float secondstofinish = (totaltimesteps-timesteps)/timeperstep;
+    Compute_float secondstofinish = ((Compute_float)(totaltimesteps-timesteps))/timeperstep;
     printf("%i timesteps / second.  Finishing in %i seconds, %i%% done\n",
             (int)timeperstep, //print everything as ints to make the display a little nicer - don't need perfect accuraccy
             (int)secondstofinish,
-            (int)((Compute_float)timesteps/totaltimesteps*100.0));
+            (int)((Compute_float)timesteps/(Compute_float)totaltimesteps*100.0));
 
 }

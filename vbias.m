83dd18db0672e2d89691a17f1f33f687626035f9
john palmer <j.palmer@physics.usyd.edu.au>
2016-08-05 13:07:35 +1000

    update vbias

--- vbias.m
+++ vbias.m
@@ -5,5 +5,8 @@ fname='output/0.txt';
 data=importdata(fname);
 block(:,:) = data((trialnum*200+1):(trialnum*200+100),(center-range):(center+range));
 imagesc(center-range:center+range,1:100,block) %plot with rescaled axis
-csvwrite('out.txt',block)
+bmin=min(min(block))
+bmax=max(max(block))
+block=(block-bmin)/(bmax-bmin)*length(jet);
+imwrite(block,jet,'vbias.png')
 colorbar
